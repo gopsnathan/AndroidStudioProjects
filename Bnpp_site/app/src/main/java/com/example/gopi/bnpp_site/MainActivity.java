ba01732d887c6da64043633b05aaef002d92c114
Gopi <gopinathan.mariappan@bnpparibasfortis.com>
2016-10-28 16:07:11 +0200

    Creating new android repo

---
+++ Bnpp_site/app/src/main/java/com/example/gopi/bnpp_site/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.gopi.bnpp_site;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://easybanking.qabnpparibasfortis.be/");
+
+    }
+}
